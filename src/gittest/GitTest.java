aa6f7e6e74cf64621ad79faf356f8bc4e5af0619
Hieu Duc Nguyen <ndhieu.bk@gmail.com>
2019-01-19 11:27:09 +0700

    tsete

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -15,9 +15,10 @@ public class GitTest {
 	 */
 	public static void main(String[] args) {
 		System.out.println("hello world");
+                test();
 	}
 
-	public void test() {
+	public static void test() {
 		System.out.println("hello test");
 	}
 
