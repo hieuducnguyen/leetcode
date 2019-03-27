dd5a4418ad1389ad9255327676283d90619375c4
Hieu Duc Nguyen <ndhieu.bk@gmail.com>
2019-03-27 13:52:58 +0700

    lis

---
+++ src/top100like/medium/Search2DMatrix.java
@@ -0,0 +1,26 @@
+/*
+ *  Copyright (c) 2012-2018 by Zalo Group.
+ *  All Rights Reserved.
+ */
+package top100like.medium;
+
+/**
+ *
+ * @author hieund3
+ */
+public class Search2DMatrix {
+
+	private static boolean process(int[][] matrix) {
+		return false;
+	}
+
+	public static void main(String[] args) {
+//		int[][] matrix = new int[][]{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19},
+//		{3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}};
+//		boolean ret = process(matrix);
+//		System.out.println("arr: " + matrix[4][1]);
+//		System.out.println("ret: " + ret);
+		
+	}
+
+}
