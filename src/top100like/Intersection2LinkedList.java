334ceb3f81dabea0a19c30ea01fd5b25124cc938
Hieu Duc Nguyen <ndhieu.bk@gmail.com>
2019-04-01 14:12:30 +0700

    lis

--- src/top100like/Intersection2LinkedList.java
+++ src/top100like/Intersection2LinkedList.java
@@ -13,94 +13,94 @@ import java.util.Set;
  */
 public class Intersection2LinkedList {
 
-    public static class ListNode {
+	public static class ListNode {
 
-        int val;
-        ListNode next;
+		int val;
+		ListNode next;
 
-        ListNode(int x) {
-            val = x;
-            next = null;
-        }
-    }
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
 
-    public static ListNode process(ListNode headA, ListNode headB) {
-        ListNode tmpA = headA, tmpB;
-        while (tmpA != null) {
-            tmpB = headB;
-            while (tmpB != null) {
-                if (tmpA == tmpB) {
-                    return tmpA;
-                }
-                tmpB = tmpB.next;
-            }
-            tmpA = tmpA.next;
-        }
-        return null;
-    }
+	public static ListNode process(ListNode headA, ListNode headB) {
+		ListNode tmpA = headA, tmpB;
+		while (tmpA != null) {
+			tmpB = headB;
+			while (tmpB != null) {
+				if (tmpA == tmpB) {
+					return tmpA;
+				}
+				tmpB = tmpB.next;
+			}
+			tmpA = tmpA.next;
+		}
+		return null;
+	}
 
-    public static ListNode process2(ListNode headA, ListNode headB) {
-        Set<ListNode> set = new HashSet<>();
-        ListNode tmpA = headA, tmpB = headB;
-        while (tmpA != null || tmpB != null) {
-            if (set.contains(tmpA)) {
-                return tmpA;
-            }
-            if (tmpA != null) {
-                set.add(tmpA);
-                tmpA = tmpA.next;
+	public static ListNode process2(ListNode headA, ListNode headB) {
+		Set<ListNode> set = new HashSet<>();
+		ListNode tmpA = headA, tmpB = headB;
+		while (tmpA != null || tmpB != null) {
+			if (set.contains(tmpA)) {
+				return tmpA;
+			}
+			if (tmpA != null) {
+				set.add(tmpA);
+				tmpA = tmpA.next;
 
-            }
-            if (set.contains(tmpB)) {
-                return tmpB;
-            }
-            if (tmpB != null) {
-                set.add(tmpB);
-                tmpB = tmpB.next;
-            }
-        }
-        return null;
-    }
+			}
+			if (set.contains(tmpB)) {
+				return tmpB;
+			}
+			if (tmpB != null) {
+				set.add(tmpB);
+				tmpB = tmpB.next;
+			}
+		}
+		return null;
+	}
 
-    public static ListNode process3(ListNode headA, ListNode headB) {
-        HashSet<ListNode> set = new HashSet<>();
-        ListNode tmpA = headA;
-        while (tmpA != null) {
-            set.add(tmpA);
-            tmpA = tmpA.next;
-        }
-        ListNode tmpB = headB;
-        while (tmpB != null) {
-            if (set.contains(tmpB)) {
-                return tmpB;
-            }
-            tmpB = tmpB.next;
-        }
-        return null;
-    }
+	public static ListNode process3(ListNode headA, ListNode headB) {
+		HashSet<ListNode> set = new HashSet<>();
+		ListNode tmpA = headA;
+		while (tmpA != null) {
+			set.add(tmpA);
+			tmpA = tmpA.next;
+		}
+		ListNode tmpB = headB;
+		while (tmpB != null) {
+			if (set.contains(tmpB)) {
+				return tmpB;
+			}
+			tmpB = tmpB.next;
+		}
+		return null;
+	}
 
-    public static ListNode process4(ListNode headA, ListNode headB) {
-        
-    }
+	public static ListNode process4(ListNode headA, ListNode headB) {
+		return null;
+	}
 
-    public static void main(String[] args) {
-        ListNode a = new ListNode(1);
-        ListNode b = new ListNode(2);
-        ListNode c = new ListNode(10);
-        ListNode d = new ListNode(4);
-        ListNode e = new ListNode(5);
-        ListNode g = new ListNode(6);
-        ListNode h = new ListNode(7);
-        ListNode k = new ListNode(8);
-        a.next = b;
-        b.next = c;
-        g.next = h;
-        h.next = k;
-        k.next = c;
-        c.next = d;
-        d.next = e;
-        ListNode ret = process2(b, g);
-        System.out.println("ret: " + ret.val);
-    }
+	public static void main(String[] args) {
+		ListNode a = new ListNode(1);
+		ListNode b = new ListNode(2);
+		ListNode c = new ListNode(10);
+		ListNode d = new ListNode(4);
+		ListNode e = new ListNode(5);
+		ListNode g = new ListNode(6);
+		ListNode h = new ListNode(7);
+		ListNode k = new ListNode(8);
+		a.next = b;
+		b.next = c;
+		g.next = h;
+		h.next = k;
+		k.next = c;
+		c.next = d;
+		d.next = e;
+		ListNode ret = process2(b, g);
+		System.out.println("ret: " + ret.val);
+	}
 
 }
--- src/top100like/medium/Permutation.java
+++ src/top100like/medium/Permutation.java
@@ -4,11 +4,32 @@
  */
 package top100like.medium;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author hieund3
  */
 public class Permutation {
+
+	// note dùng queue hay vì list of list, dk dừng là queue đó có size bằng 7 (arr.length)
+	public static List<ArrayList<Integer>> lists = new ArrayList<ArrayList<Integer>>();
+
+	private static int process(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			if (lists.isEmpty()) {
+				ArrayList<Integer> list = new ArrayList<>();
+				list.add(arr[i]);
+				lists.add(list);
+			} else {
+				for (int j = 0; j < lists.size(); j++) {
+
+				}
+			}
+		}
+		return 0;
+	}
 
 	public static void main(String[] args) {
 		int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7};
