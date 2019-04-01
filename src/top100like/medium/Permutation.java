a70f6db7a76c10e384d0e064c12f114cc4fddf3c
Hieu Duc Nguyen <ndhieu.bk@gmail.com>
2019-04-01 07:48:44 +0700

    Merge origin/master
    Conflicts:
    	src/top100like/Intersection2LinkedList.java

--- src/top100like/medium/Permutation.java
+++ src/top100like/medium/Permutation.java
@@ -14,21 +14,4 @@ public class Permutation {
 		int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7};
 		int num = process(arr);
 	}
-
-	private static int process(int[] arr) {
-		if (arr == null || arr.length == 0) {
-			return 0;
-		}
-		int[] index = new int[arr.length];
-		int num = 0;
-		for (int i = 0; i < arr.length; i++) {
-			
-		}
-	}
-
-	private static void reset(int[] indexArr) {
-		for (int i = 0; i < indexArr.length; i++) {
-			indexArr[i] = 0;
-		}
-	}
 }
