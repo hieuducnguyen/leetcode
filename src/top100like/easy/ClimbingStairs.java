6d4879107d3921305b4fb0dccd8c594f286cd979
hieund3 <hieund3@hieund3-ThinkPad-T480>
2019-04-30 21:28:24 +0700

    update

--- src/top100like/easy/ClimbingStairs.java
+++ src/top100like/easy/ClimbingStairs.java
@@ -14,41 +14,57 @@ import java.util.Map;
  */
 public class ClimbingStairs {
 
-    public static Map<Integer, Integer> map = new HashMap<>();
+	public static Map<Integer, Integer> map = new HashMap<>();
 
-    public static void main(String[] args) {
-        int num = 45;
-        int numWay = process(num);
-        System.out.println("numWay: " + numWay);
-    }
+	public static void main(String[] args) {
+		int num = 45;
+		int numWay = process1(num);
+		System.out.println("numWay: " + numWay);
+	}
 
-    private static int process(int num) {
-        if (num <= 0) {
-            return 0;
-        }
-        if (num == 1) {
-            return 1;
-        }
-        if (num == 2) {
-            return 2;
-        }
-        if (map.containsKey(num)) {
-            return map.get(num);
-        }
-        int f_1, f_2;
-        if (!map.containsKey(num - 1)) {
-            f_1 = process(num - 1);
-            map.put(num - 1, f_1);
-        } else {
-            f_1 = map.get(num - 1);
-        }
-        if (!map.containsKey(num - 2)) {
-            f_2 = process(num - 2);
-            map.put(num - 2, f_2);
-        } else {
-            f_2 = map.get(num - 2);
-        }
+	private static int process(int num) {
+		if (num <= 0) {
+			return 0;
+		}
+		if (num == 1) {
+			return 1;
+		}
+		if (num == 2) {
+			return 2;
+		}
+		if (map.containsKey(num)) {
+			return map.get(num);
+		}
+		int f_1, f_2;
+		if (!map.containsKey(num - 1)) {
+			f_1 = process(num - 1);
+			map.put(num - 1, f_1);
+		} else {
+			f_1 = map.get(num - 1);
+		}
+		if (!map.containsKey(num - 2)) {
+			f_2 = process(num - 2);
+			map.put(num - 2, f_2);
+		} else {
+			f_2 = map.get(num - 2);
+		}
 
-        return f_1 + f_2;
-    }
+		return f_1 + f_2;
+	}
+
+	private static int process1(int num) {
+		int[] result = new int[num];
+		if (num == 1) {
+			return 1;
+		}
+		if (num == 2) {
+			return 2;
+		}
+		result[0] = 1;
+		result[1] = 2;
+		for (int i = 2; i < num; i++) {
+			result[i] = result[i - 1] + result[i - 2];
+		}
+		return result[num - 1];
+	}
 }
--- src/top100like/easy/MaxSubArray.java
+++ src/top100like/easy/MaxSubArray.java
@@ -77,19 +77,5 @@ public class MaxSubArray {
 		return max;
 	}
 
-	private static int process4(int[] arr) {
-		int size = arr.length;
-		int max_so_far = Integer.MIN_VALUE, max_ending_here = 0;
-
-		for (int i = 0; i < size; i++) {
-			max_ending_here = max_ending_here + arr[i];
-			if (max_so_far < max_ending_here) {
-				max_so_far = max_ending_here;
-			}
-			if (max_ending_here < 0) {
-				max_ending_here = 0;
-			}
-		}
-		return max_so_far;
-	}
+	
 }
