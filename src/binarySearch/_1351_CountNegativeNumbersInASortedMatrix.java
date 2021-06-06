dbbc474d4dc34e6f15f54a8fcab2c6447fa185e6
hieund3 <hieund3@vng.com.vn>
2021-06-06 11:28:35 +0700

    finish 1351

--- src/binarySearch/_1351_CountNegativeNumbersInASortedMatrix.java
+++ src/binarySearch/_1351_CountNegativeNumbersInASortedMatrix.java
@@ -1,34 +1,56 @@
 package binarySearch;
 
-public class _1351_CountNegativeNumbersInASortedMatrix {
-	public static final _1351_CountNegativeNumbersInASortedMatrix INSTANCE = new _1351_CountNegativeNumbersInASortedMatrix();
+import java.util.Arrays;
 
-	int countNegatives(int[][] grid) {
+public class _1351_CountNegativeNumbersInASortedMatrix {
+	public static final _1351_CountNegativeNumbersInASortedMatrix INSTANCE =
+		new _1351_CountNegativeNumbersInASortedMatrix();
+
+	public int countNegatives2(int[][] grid) {
 		if (grid == null || grid.length == 0) {
 			return 0;
 		}
-		//test 5
-		//test 6
-		int i = 0, j = grid[0].length - 1, temp, result = 0;
-		while (i < grid.length && j >= 0) {
-			temp = grid[i][j];
-			if (temp < 0) {
-				result += (grid.length - i);
-				j--;
+		int colIndex = grid[0].length - 1, rowIndex = 0, result = 0;
+		while (colIndex >= 0 && rowIndex <= grid.length - 1) {
+			if (grid[rowIndex][colIndex] < 0) {
+				result += (grid.length - rowIndex);
+				colIndex--;
 			} else {
-				i++;
+				rowIndex++;
 			}
 		}
-		//test chery pick
-		//test chery pick 2
-		// test chery pick 3
-		// test chery pick 4
-		// test chery pick 5
 		return result;
 	}
 
-	public static void main(String[] args) {
-		int[][] grid = {{4, 3, 2, -1}, {3, 2, 1, -1}, {1, 1, -1, -2}, {-1, -1, -2, -3}};
-		System.out.println("\"test\" = " + "test");
+	public int countNegatives(int[][] grid) {
+		if (grid == null || grid.length == 0) {
+			return 0;
+		}
+		int result = 0, preFirstNeg = grid[0].length;
+		for (int[] ints : grid) {
+			preFirstNeg = binSearchFirstNegative(Arrays.copyOfRange(ints, 0, preFirstNeg));
+			result += (grid[0].length - preFirstNeg);
+		}
+		return result;
+
+	}
+
+	private boolean hasNegative(int[] ints, int firstNegative) {
+		return firstNegative != ints.length;
+	}
+
+	int binSearchFirstNegative(int[] arr) {
+		int r = 0, l = arr.length, m;
+		while (r < l) {
+			m = (r + l) / 2;
+			if (arr[m] < 0 && (m == 0 || arr[m - 1] >= 0)) {
+				return m;
+			} else if (arr[m] >= 0) {
+				r = m + 1;
+			} else {
+				l = m;
+			}
+		}
+		return l;
 	}
 }
